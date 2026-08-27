2ebe7b3cca0107fc2ff54fb04dff3cf81c355896
Adrian Kolev <adrian.kolev@lecturio.de>
2026-08-27 22:01:47 +0300

    Hide prerendered HTML until React mounts so refresh no longer flashes unstyled text.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { flushSync } from "react-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootEl = document.getElementById("root")!;
+flushSync(() => {
+  createRoot(rootEl).render(<App />);
+});
+rootEl.setAttribute("data-ready", "");
